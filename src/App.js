52c72e51cc1ce19bb4f761de973c6cbddaf4bc77
TAMIM <saifur.tamim@gmail.com>
2021-09-19 22:20:57 +0600

    final

--- src/App.js
+++ src/App.js
@@ -30,33 +30,37 @@ const Countries = () => {
   }, 1000)
 
   function getCountries() {
+    setCountries([]);
     let searched = document.getElementById('searchInput').value;
     if (searched === '') { searched = 'Bangladesh' };
     const url = `https://restcountries.eu/rest/v2/name/${searched}`;
     fetch(url).then(r => r.json()).then(d => setCountries(d));
   }
 
-  return (
-    countries.map(country => {
-      return (
-        <div className="country">
-          <img src={country.flag} />
-          <h3 style={{ margin: '10px 0' }}>
-            {country.name} ({country.nativeName})
-          </h3>
-          <p style={{ margin: '0', fontSize: '17px' }}>
-            <b>Region:</b> {country.region} <br />
-            <b>Sub-region:</b> {country.subregion} <br />
-            <b>Capital:</b> {country.capital} <br />
-            <b>Population:</b> {country.population} <br />
-            <b>Languages:</b> {country.languages.map(lg => lg.name).join(', ')} <br />
-            <b>Currencies:</b> {country.currencies.map(currency => currency.name + '(' + currency.code + ')').join(', ')} <br />
-            <b>Calling-codes:</b> {country.callingCodes.join(', ')}
-          </p>
-        </div>
-      )
-    })
-  )
+  if (countries.length > 0) {
+    return (
+      countries.map(country => {
+        return (
+          <div className="country">
+            <img src={country.flag} />
+            <h3 style={{ margin: '10px 0' }}>
+              {country.name} ({country.nativeName})
+            </h3>
+            <p style={{ margin: '0', fontSize: '17px' }}>
+              <b>Region:</b> {country.region} <br />
+              <b>Sub-region:</b> {country.subregion} <br />
+              <b>Capital:</b> {country.capital} <br />
+              <b>Population:</b> {country.population} <br />
+              <b>Languages:</b> {country.languages.map(lg => lg.name).join(', ')} <br />
+              <b>Currencies:</b> {country.currencies.map(currency => currency.name + '(' + currency.code + ')').join(', ')} <br />
+              <b>Calling-codes:</b> {country.callingCodes.join(', ')}
+            </p>
+          </div>
+        )
+      })
+    )
+  } else if (countries.length === 0) { return <h2>Loading...</h2> }
+  else if (countries.status === 404) { return <h2>Nothing found..!</h2> }
 }
 
 export default App;
